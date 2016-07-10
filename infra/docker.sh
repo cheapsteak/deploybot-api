060b854819db72b89d0cbeb9dd42b99e82665256
Shane Wilson <shane.wilson@oicr.on.ca>
2016-07-10 12:00:34 -0400

    [DPLY-4] Filter deployed containers
    - Only show GDC containers in list of deployed containers
    - update docker script to push develop -> :latest and master -> :stable

--- infra/docker.sh
+++ infra/docker.sh
@@ -9,6 +9,16 @@ if [ $TRAVIS_PULL_REQUEST = 'false' ]; then
   docker tag deploybot-api quay.io/ncigdc/deploybot-api:${TRAVIS_BRANCH/\//-} \
     && docker push quay.io/ncigdc/deploybot-api:${TRAVIS_BRANCH/\//-};
 
+  if [ $TRAVIS_BRANCH = 'develop' ]; then
+    docker tag deploybot-api quay.io/ncigdc/deploybot-api:latest \
+      && docker push quay.io/ncigdc/deploybot-api:latest;
+  fi
+
+  if [ $TRAVIS_BRANCH = 'master' ]; then
+    docker tag deploybot-api quay.io/ncigdc/deploybot-api:stable \
+      && docker push quay.io/ncigdc/deploybot-api:stable;
+  fi
+
   # If this commit has a tag, use on the registry too.
   if ! test -z $TRAVIS_TAG; then
     docker tag deploybot-api quay.io/ncigdc/deploybot-api:${TRAVIS_TAG} \
